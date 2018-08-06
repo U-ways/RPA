9e9dcea4a126028dabc5a8c398d2736e4bf9c42a
u-ways <ue95.career@gmail.com>
2018-08-06 20:01:54 +0100

    add documentation and change anonymous function to named ones.

--- rpa/app/mvc/controllers/index.js
+++ rpa/app/mvc/controllers/index.js
@@ -5,16 +5,27 @@ import express from 'express';
 
 const  router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', getLogic);
 
+export default router;
+
+/* logic
+============================================================================= */
+
+/**
+ * Render the home page.
+ *
+ * @param  {request}   req   request object
+ * @param  {response}  res   response object
+ * @param  {Function}  next  callback to the next middleware
+ * @return {response}        render login page on success, error resposne otherwise.
+ */
+function getLogic (req, res, next) {
   let view = {
-    title: 'RPA',
+    title: 'Homepage',
     message: res.locals.message,
   };
 
   res.status(res.locals.status || 200);
   res.render('index', view);
-});
-
-export default router;
+}
