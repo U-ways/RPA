eb620218a59341d93456d5979edad44553ebb687
u-ways <ue95.career@gmail.com>
2018-08-06 17:03:06 +0100

    rename to sessionGarbageCollector and change functionality

--- rpa/app/middleware/sessionGarbageCollector.js
+++ rpa/app/middleware/sessionGarbageCollector.js
@@ -11,44 +11,46 @@
  *
  * **Usage Example** - whenever you need to do a response redirection and need
  * to pass an error or a feedback to the redirected URL pass the values as a
- * session property like so:
+ * session `temp` property like so:
  *
  * ```js
- * req.session.error = {
+ * req.session.temp = {
  *   status: 400,
  *   message: 'user already logged out: no active session found to destroy.'
  * };
  *
- * req.session.feedback = {
+ * req.session.temp = {
  *   status: 200,
  *   message: 'success: you\'ve securely logged out.'
  * };
  * ```
  *
- * And you will be able to use those values by accessing response's `locals`
- * object like normal.
+ * And access the values within response `locals`
+ * object **without** using the `temp` property:
+ *
+ * ```js
+ * let error    = res.locals.error;
+ * let feedback = res.locals.feedback;
+ * ```
  *
  * @param  {request}   req   request object
  * @param  {response}  res   response object
  * @param  {Function}  next  callback to the next middleware
  * @return {Function}        returns the request to the next middleware when done.
  */
-export function preserveLocalsOnRedirection (req, res, next) {
-  if (req.session.error) {
-    res.locals.status  = req.session.error.status;
-    res.locals.message = req.session.error.message;
-
-    delete req.session.error;
+export function sessionGarbageCollector (req, res, next) {
+  /** pass each object to temp to `res.locals` */
+  if (req.session.temp) {
+    let temp = req.session.temp;
+    for (let key of Object.keys(temp)) {
+      res.locals[key] = temp[key];
+    }
+    /**
+     * Allow property `temp` to be eligible for garbage collection
+     * by setting its value to null.
+     */
+    req.session.temp = null;
     return next();
   }
-
-  if (req.session.feedback) {
-    res.locals.status  = req.session.feedback.status;
-    res.locals.message = req.session.feedback.message;
-
-    delete req.session.feedback;
-    return next();
-  }
-
   return next();
 }
