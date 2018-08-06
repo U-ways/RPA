0f0bbaf09cdf7671edc7a98de083a4696332a847
u-ways <ue95.career@gmail.com>
2018-08-06 02:31:44 +0100

    add index controller logic

--- rpa/app/mvc/controllers/index.js
+++ rpa/app/mvc/controllers/index.js
@@ -1,13 +1,19 @@
+/* index controller
+============================================================================= */
+
 import express from 'express';
+
 const  router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  if (req.session) {
-    console.log(req.session.id);
-    console.log(req.session.user);
-  }
-  res.render('index', { title: 'RPA' });
+router.get('/', (req, res, next) => {
+  let view = {
+    title: 'RPA',
+    message: res.locals.message
+  };
+
+  res.status(res.locals.status || 200);
+  res.render('index', view);
 });
 
 export default router;
