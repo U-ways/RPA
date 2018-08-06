4bd081fcdd769e19aae320055ed413f07b7d64da
u-ways <ue95.career@gmail.com>
2018-08-06 02:31:19 +0100

    add login controller logic

--- rpa/app/mvc/controllers/login.js
+++ rpa/app/mvc/controllers/login.js
@@ -1,73 +1,67 @@
 /* login controller
 ============================================================================= */
+
 import express from 'express';
 import { UserModel } from '../models/User.js';
+import { authenticateUser } from '../../middleware/authenticateUser.js';
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('login', { title: 'Login' });
+router.get('/', (req, res) => {
+  let view = {
+    title: 'Login',
+    message: res.locals.message
+  };
+
+  res.status(res.locals.status || 200);
+  res.render('login', view);
 });
 
-/** **/
+router.post('/', authenticateUser, login);
 
-function authenticateUser (req, res, next) {
-  let username = req.body.username,
-      password = req.body.password,
-      email    = req.body.email;
-  let conditions = !!username ? {username: username} : {email: email};
+/* logic
+============================================================================= */
 
-  UserModel.findOne(conditions, (err, user) => {
-    if (err)   return next(err);
-    if (!user) {
-      let input = Object.keys(conditions);
-      let error = new Error(`Incorrect ${input}, please try again.`);
-      error.name = 'Authentication failed';
-      error.status = 401;
-      return next(error);
-    };
+/**
+ * Creates a new session and set the session as authenticated.
+ *
+ * NOTE: For security reasons, If the session is unable to regenerate,
+ * the user won't be authenticated and receive an error response.
+ *
+ * @param  {request}   req   request object
+ * @param  {response}  res   response object
+ * @param  {Function}  next  callback to the next middleware
+ * @return {Function}        pass the request to the next middleware on success.
+ *                           pass to error middleware on failure otherwise.
+ */
+function login (req, res, next) {
+  let user = req.locals.user;
 
-    return user.validPassword(password, user.password)
-    .then(match => {
-      if (match) {
-        req.locals = {
-          user: {
-            id: user._id,
-            username: user.username,
-            email:    user.email
-          }
-        }
-        return next();
-      } else {
-        let error = new Error(`Incorrect password, please try again.`);
-        error.name = 'Authentication failed';
-        error.status = 401;
-        return next(error);
+  /** create new session for authenticated user */
+  req.session.regenerate(err => {
+    if (err) {
+      return res.status(500)
+        .json({ error: 'Unable to create a new session for authenticated user.' });
+    }
+
+    req.session.auth = true;
+    req.session.user = {
+      id: user._id,
+      username: user.username,
+      email: user.email
+    }
+    /** increase session age to 30 minutes for authenticated users */
+    req.session.cookie.maxAge = 30 * 60 * 1000;
+
+    /** log user activity and then redirect to dashboard */
+    user.logs.push({activity: 0});
+    user.save().then(user => {
+      req.session.feedback = {
+        message: `welcome back ${user.username}, last login: [find last login using API].`
       };
-    })
-    .catch(err => {
-      let error = new Error(
-        'Sorry, we\'re unable to validated your password.\n' +
-        'This error had been logged and our staff will investigate the problem asap.');
-      error.name = 'Internal Server Error';
-      error.status = 500;
-      error.caught = err;
-      return next(error);
+      return res.redirect('/dashboard');
     });
-
   });
 }
-
-router.post('/', authenticateUser, (req, res, next) => {
-  console.log('you\'ve logged in!');
-  // Create new session for authenticated user
-  req.session.regenerate(err => {
-    if (err) return next(err);
-    req.session.auth = true;
-    req.session.user = req.locals.user;
-    req.session.cookie.maxAge = 30 * 60 * 1000, // increase to 30 minutes
-    res.redirect('/dashboard');
-  })
-});
 
 export default router;
