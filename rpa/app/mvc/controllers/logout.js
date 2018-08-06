0dccb9ccb5b06ef0ca1300aa44a9fdced13c45d3
u-ways <ue95.career@gmail.com>
2018-08-06 02:31:32 +0100

    add logout controller logic

--- rpa/app/mvc/controllers/logout.js
+++ rpa/app/mvc/controllers/logout.js
@@ -1,21 +1,52 @@
 /* logout controller
 ============================================================================= */
+
 import express from 'express';
+import { UserModel } from '../models/User.js';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  if (req.session.id) {
-    req.session.destroy(() => {
-      console.log('session destroyed');
-      res.redirect('/');
-    });
-  } else {
-    let error = new Error(`User already logged out; No session found to destroy.`);
-    error.name = 'Bad Request';
-    error.status = 400;
-    return next(error);
+router.get('/', destorySession);
+
+/* logic
+============================================================================= */
+
+/**
+ * Destroys the session and will unset the `req.session property`.
+ * This middleware should be called whenever the user wants to log out.
+ *
+ * @param  {request}   req   request object
+ * @param  {response}  res   response object
+ * @param  {Function}  next  callback to the next middleware
+ * @return {Function}        pass the request to the next middleware on success.
+ *                           pass to error middleware on failure otherwise.
+ */
+function destorySession (req, res, next) {
+  if (!req.session.user) {
+    req.session.error = {
+      status: 400,
+      message: 'user already logged out: no active session found to destroy.'
+    };
+    return res.redirect('/');
   }
-});
+
+  let id = req.session.user.id;
+  req.session.regenerate(() => {
+    /** find the user with the active session */
+    UserModel.findById(id).then(
+      user => {
+        /** log user activity and then redirect to dashboard */
+        user.logs.push({ activity: 1 });
+        user.save().then(user => {
+          req.session.feedback = {
+            status: 200,
+            message:'success: you\'ve securely logged out.'
+          }
+          return res.redirect('/');
+        });
+      }
+    );
+  });
+}
 
 export default router;
