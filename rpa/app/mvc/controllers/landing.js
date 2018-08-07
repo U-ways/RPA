a6f4de9005026061030263cf27108c685a9c8c27
u-ways <ue95.career@gmail.com>
2018-08-07 21:14:36 +0100

    rename to landing

---
+++ rpa/app/mvc/controllers/landing.js
@@ -0,0 +1,46 @@
+/* index controller
+============================================================================= */
+
+import path from 'path';
+import express from 'express';
+
+import { recaptcha } from '../../middleware/recaptcha.js';
+
+const router = express.Router();
+
+router.get('/', recaptcha.middleware.render, getLogic);
+
+/* logic
+============================================================================= */
+
+/** get current file name and remove extension */
+const FILE_NAME = path.basename(__filename).slice(0, -3);
+
+/**
+ * Render the landing page.
+ *
+ * @param  {request}   req   request object
+ * @param  {response}  res   response object
+ * @param  {Function}  next  callback to the next middleware
+ * @return {response}        render landing page on success, error resposne otherwise.
+ */
+function getLogic (req, res, next) {
+  let view = {
+    title: FILE_NAME,
+    stylesheets: [
+      'iconfont/material-icons.css',
+      'stylesheets/core.css',
+      `stylesheets/${FILE_NAME}.css`
+    ],
+    scripts: [
+      'scripts/login.js',
+      `scripts/${FILE_NAME}.js`
+    ],
+    message: res.locals.message,
+    captcha: res.recaptcha
+  };
+
+  return res.render(FILE_NAME, view);
+}
+
+export default router;
