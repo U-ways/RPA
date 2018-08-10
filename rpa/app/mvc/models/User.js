83ab429a69a05734642a1149b96ad500555f3fcc
u-ways <ue95.career@gmail.com>
2018-08-10 02:21:58 +0100

    add createLog and getLastLogin instance methods

--- rpa/app/mvc/models/User.js
+++ rpa/app/mvc/models/User.js
@@ -75,14 +75,58 @@ UserSchema.pre('save', function (next) {
 import bcrypt from 'bcrypt';
 
 /**
+ * Creates an activity log for the user
+ *
+ * @param  {String} type          activity type based on enum
+ * @param  {String} [description] optional description of activity
+ * @return {promise}
+ */
+function createLog (type, description) {
+  let user = this;
+
+  /** create activity type as enum for easier reference */
+  let activity = {
+    LOGIN:  0, LOGOUT: 1,
+    CREATE: 2, READ:   3,
+    UPDATE: 4, DELETE: 5,
+  };
+
+  user.logs.push({
+    activity: activity[type],
+    description: description
+  });
+
+  return user.save();
+}
+
+/**
+ * Get user's last login
+ *
+ * @return {String}  user's last login session date in UTC format
+ */
+function getLastLogin () {
+  /** sort a shallow copy of user's logs */
+  let logs = this.logs.slice();
+
+  /** sort LOGIN logs ascending based on their dates */
+  logs.sort((a,b) => {
+    if (a.activity == 0 && b.activity != 0) return -1;
+    if (a.activity != 0 && b.activity == 0) return  1;
+    return (a.date > b.date) ? -1 : 1;
+  });
+
+  /** return user's last login session date */
+  return new Date(logs[1].date).toUTCString();
+}
+
+/**
  * A middleware to automatically hash the password
  * before it's saved to the database.
  *
  * @param  {String}  password  clear password
  * @return {String}            hashed password
  */
-
-function bcryptHash(password) {
+function bcryptHash (password) {
   const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS);
   return bcrypt.hash(password, SALT_ROUNDS);
 }
@@ -94,11 +138,12 @@ function bcryptHash(password) {
  * @return {boolean}           true if password match, false otherwise.
  *                             also returns false if errors found.
  */
-
-function comaprePassword(password, hash) {
+function comaprePassword (password, hash) {
   return bcrypt.compare(password, hash);
 }
 
+UserSchema.methods.createLog     = createLog;
+UserSchema.methods.getLastLogin  = getLastLogin;
 UserSchema.methods.hashPassword  = bcryptHash;
 UserSchema.methods.validPassword = comaprePassword;
 
