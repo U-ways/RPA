97d0b85f471e7b359673211dabeac1650f06489a
u-ways <ue95.career@gmail.com>
2018-08-12 01:55:11 +0100

    rename and use as a flash messages middleware

--- rpa/app/middleware/flashMessages.js
+++ rpa/app/middleware/flashMessages.js
@@ -1,6 +1,8 @@
 /**
- * This is a middleware to allow passing user session errors and feedback for
- * the response's `locals` object (res#locals) on redirection.
+ * This is a middleware to allow passing user session flash errors and feedback
+ * form the response's `locals` object (res#locals) on redirection.
+ *
+ * This method is also known as "flash messaging"
  *
  * **Problem** - whenever you redirect as a response, you start a new request,
  * and thus the response and request `locals` are all reset.
@@ -11,26 +13,25 @@
  *
  * **Usage Example** - whenever you need to do a response redirection and need
  * to pass an error or a feedback to the redirected URL pass the values as a
- * session `temp` property like so:
+ * session `flash` property like so:
  *
  * ```js
- * req.session.temp = {
+ * req.session.flash = {
  *   status: 400,
  *   message: 'user already logged out: no active session found to destroy.'
  * };
  *
- * req.session.temp = {
+ * req.session.flash = {
  *   status: 200,
  *   message: 'success: you\'ve securely logged out.'
  * };
  * ```
  *
- * And access the values within response `locals`
- * object **without** using the `temp` property:
+ * And access the values within response `locals` as if they belonged there:
  *
  * ```js
- * let error    = res.locals.error;
- * let feedback = res.locals.feedback;
+ * let error    = res.locals.flash.error;
+ * let feedback = res.locals.flash.feedback;
  * ```
  *
  * @param  {request}   req   request object
@@ -38,18 +39,23 @@
  * @param  {Function}  next  callback to the next middleware
  * @return {Function}        returns the request to the next middleware when done.
  */
-export function sessionGarbageCollector (req, res, next) {
-  /** pass each object to temp to `res.locals` */
-  if (req.session && req.session.temp) {
-    let temp = req.session.temp;
-    for (let key of Object.keys(temp)) {
-      res.locals[key] = temp[key];
+export function flashMessages (req, res, next) {
+  /** pass each object in flash to `res.locals` */
+  if (req.session.flash) {
+    let temp  = {};
+    let flash = req.session.flash;
+
+    for (let key of Object.keys(flash)) {
+      temp[key] = flash[key];
     }
+
+    res.locals.flash = temp;
     /**
-     * Allow property `temp` to be eligible for garbage collection
+     * Allow property `flash` to be eligible for garbage collection
      * by setting its value to null.
      */
-    req.session.temp = null;
+    req.session.flash = null;
+    // IDEA: if no user destory the whole session?
     return next();
   }
   return next();
