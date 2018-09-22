c5dbbfbdb16dc4517d68ed95f6bbd6ceb830d99c
u-ways <ue95.career@gmail.com>
2018-09-23 00:44:55 +0100

    dismiss flash messages on click

--- rpa/app/public/scripts/core.js
+++ rpa/app/public/scripts/core.js
@@ -5,14 +5,30 @@
 const sidebar = document.querySelector('#sidebar');
 const chevron = document.querySelector('#chevron');
 
-chevron.addEventListener("click", toggleSidebar);
+chevron.addEventListener('click', toggleSidebar);
 
+// used by lnading.js
 function toggleSidebar () {
   let icon = chevron.querySelector('i');
 
-  icon.classList.toggle("fa-chevron-right");
-  icon.classList.toggle("fa-chevron-left");
+  icon.classList.toggle('fa-chevron-right');
+  icon.classList.toggle('fa-chevron-left');
 
-  sidebar.classList.toggle("open");
-  sidebar.classList.toggle("close");
+  sidebar.classList.toggle('open');
+  sidebar.classList.toggle('close');
 }
+
+let flashMsg = document.querySelector('#flash-message')
+
+flashMsg.addEventListener('click', () => {
+  flashMsg.classList.toggle('hide');
+});
+
+function toggleFlashIcons () {
+  let icon = flashMsg.querySelector('i');
+  icon.classList.toggle('fa-times-circle');
+  icon.classList.toggle('fa-exclamation-circle');
+}
+
+flashMsg.addEventListener('mouseenter', toggleFlashIcons);
+flashMsg.addEventListener('mouseleave', toggleFlashIcons);
