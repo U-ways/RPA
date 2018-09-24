dfc171b4c5dd0d853de34a6988a9c61d53dea404
u-ways <ue95.career@gmail.com>
2018-09-24 14:06:03 +0100

    add client-side fetch navigation

--- rpa/app/public/scripts/core.js
+++ rpa/app/public/scripts/core.js
@@ -2,6 +2,8 @@
   - core const elemnts will be shared and used globally with other scripts
 ============================================================================= */
 
+const head    = document.querySelector('#head');
+const main    = document.querySelector('#sub-container');
 const sidebar = document.querySelector('#sidebar');
 const chevron = document.querySelector('#chevron');
 
@@ -32,3 +34,75 @@ function toggleFlashIcons () {
 
 flashMsg.addEventListener('mouseenter', toggleFlashIcons);
 flashMsg.addEventListener('mouseleave', toggleFlashIcons);
+
+/* navigate
+============================================================================= */
+
+async function navigate (section) {
+  try {
+    /** fetch section html */
+    let data = await fetch(`./app/${section}`, {
+      method: 'GET',
+      headers: { 'Accept': 'application/json' }
+    })
+    .then(response => {
+      return response.json()
+      .then(data => {
+        if (response.ok) return data;
+        else             return Promise.reject({ status: response.status, data });
+      });
+    });
+
+    /** update window title */
+    let title  = head.querySelector('title');
+    title.innerHTML = `RPA | ${section}`;
+
+    /** update section content */
+    main.innerHTML = data.html;
+
+    /** update section heading */
+    let heading = document.querySelector('#topbar h1');
+    heading.innerHTML = section;
+
+    /* update section stylesheets */
+    let styles = head.querySelectorAll('link[data-type=section-style]');
+    if (styles !== null) {
+      styles.forEach(style => {
+        style.remove();
+      })
+    }
+    data.meta.stylesheets.forEach(href => {
+      let style  = document.createElement('link');
+      style.rel  = 'stylesheet';
+      style.href = href;
+      style.dataset.type = 'section-style';
+      head.appendChild(style);
+    });
+
+    /* update section scripts */
+    let scripts = head.querySelectorAll('script[data-type=section-script]');
+    if (scripts !== null) {
+      scripts.forEach(script => {
+        script.remove();
+      })
+    }
+    data.meta.scripts.forEach(src => {
+      let script   = document.createElement('script');
+      script.type  = 'text/javascript';
+      script.src   = src;
+      script.defer = 'defer';
+      script.dataset.type = 'section-script';
+      head.appendChild(script);
+    });
+
+    return true;
+  }
+  catch (error) {
+    // TODO: add a better error handler
+    main.innerHTML = 'Not found, sorry...'
+    console.log('Unable to fetch content:');
+    console.error(error);
+
+    return false;
+  }
+}
