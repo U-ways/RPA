fd3be474ad3e66a10676d1ea69d6f93b9c4a3c13
u-ways <ue95.career@gmail.com>
2018-09-24 13:58:52 +0100

    Changelog: - add homepage index.js as entry point for webpage controllers and routes. - move main homepage controllers to homepage folder to separate the homepage from the app.

--- rpa/app/mvc/controllers/homepage/index.js
+++ rpa/app/mvc/controllers/homepage/index.js
@@ -1,14 +1,16 @@
 /* index controller
 ============================================================================= */
 
-import path from 'path';
+// import path from 'path';
 
 import { Router    } from 'express';
-import { recaptcha } from '../../middleware/recaptcha.js';
+import { recaptcha } from '../../../middleware/recaptcha.js';
+import { blockAuthUsers } from '../../../middleware/blockAuthUsers.js';
 
 const router = Router();
 
 router.get('/',
+  blockAuthUsers,
   recaptcha.invisible.render,
   getLogic,
 );
@@ -17,7 +19,7 @@ router.get('/',
 ============================================================================= */
 
 /** get current file name and remove extension */
-const fileName = path.basename(__filename).slice(0, -3);
+// const fileName = path.basename(__filename).slice(0, -3);
 
 /**
  * Render the landing page.
@@ -28,22 +30,25 @@ const fileName = path.basename(__filename).slice(0, -3);
  * @return {response}        render landing page on success, error resposne otherwise.
  */
 function getLogic (req, res, next) {
-  let view = {
-    title: fileName,
-    stylesheets: [
-      `stylesheets/${fileName}.css`
-    ],
-    scripts: [
-      `scripts/${fileName}.js`,
-      'scripts/login.js',
-    ],
-    flash:   res.locals.flash,
-    message: res.locals.message,
-    session: req.session,
-    recaptcha: res.locals.recaptcha,
-  };
+  return res.render('homepage/about', (err, content) => {
+    let view = {
+      title: 'homepage',
+      stylesheets: [
+        'stylesheets/homepage.css',
+        'stylesheets/about.css',
+      ],
+      scripts: [
+        'scripts/homepage.js',
+        'scripts/login.js',
+      ],
+      content: content,
+      flash:   res.locals.flash,
+      session: req.session,
+      recaptcha: res.locals.recaptcha,
+    };
 
-  return res.render(fileName, view);
+    return res.render('homepage/index' , view);
+  });
 }
 
 export default router;
