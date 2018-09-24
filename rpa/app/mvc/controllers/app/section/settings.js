b8570a40b94a669a8df60016c5479239c5f1c42e
u-ways <ue95.career@gmail.com>
2018-09-24 16:23:37 +0100

    init section controller

---
+++ rpa/app/mvc/controllers/app/section/settings.js
@@ -0,0 +1,52 @@
+/* settings controller
+============================================================================= */
+
+import path from 'path';
+
+import { Router } from 'express';
+import { blockNonAuthUsers } from '../../../../middleware/blockNonAuthUsers.js';
+
+const router = Router();
+
+router.get('/',
+  blockNonAuthUsers,
+  getLogic,
+);
+
+/* logic
+============================================================================= */
+
+/** get current file name and remove extension */
+const fileName = path.basename(__filename).slice(0, -3);
+
+/**
+ * Render the settings.
+ *
+ * @param  {request}   req   request object
+ * @param  {response}  res   response object
+ * @param  {Function}  next  callback to the next middleware
+ * @return {response}        render settings on success, error resposne otherwise
+ */
+function getLogic (req, res, next) {
+  let view = {
+    // nothing needed yet
+  };
+
+  return res.render(`app/${fileName}`, view, (err, html) => {
+    // stylesheets and scripts to append to the <head>
+    let meta = {
+      stylesheets: [
+        `stylesheets/app/${fileName}.css`
+      ],
+      scripts: [
+        `scripts/app/${fileName}.js`,
+      ],
+      flash: res.locals.flash,
+    };
+
+    return res.json({ html, meta });
+  });
+
+}
+
+export default router;
