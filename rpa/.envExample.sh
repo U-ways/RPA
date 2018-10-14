5cafb4b11703b10a68c605ef317ab8f90d75470c
u-ways <ue95.career@gmail.com>
2018-10-14 19:43:00 +0100

    update .envExample.sh

--- rpa/.envExample.sh
+++ rpa/.envExample.sh
@@ -13,7 +13,30 @@ HOST="localhost"
 HTTP_PORT="3000"
 REDIS_PORT="6379"
 
-## database uri + username & password
+## Admin account
+#   root account within RPA that always exist
+ADMIN_USERNAME="admin"
+ADMIN_EMAIL="admin@rpa.com"
+ADMIN_PASSWORD="root"
+
+## Bot account
+#   bot account to verify emails
+BOT_USERNAME="RPA-Bot"
+BOT_PASSWORD="bot"
+BOT_EMAIL="no-reply@rpa.com"
+
+## bcrypt hash salt rounds
+#   see: https://stackoverflow.com/a/46713082/5037430
+SALT_ROUNDS="12"
+
+## session cookie secret (used to sign the session ID cookie)
+#   @see:  https://github.com/expressjs/session#secret
+#   256-bit WEP Keys: https://randomkeygen.com/
+SECRET_1="99F557F87E1CFD7CF5AB23F21E7D1"
+SECRET_2="5DC554673B99DD4B83BF8E766E722"
+SECRET_3="496A7AE21CDF3EC67217DAD89A1B1"
+
+## database uri + username & password (replace USER,PASSWORD and DATABASE)
 #   development
 DEV_DB_URI_USER="mongodb+srv://USER:PASSWORD@prototype-db-hituj.mongodb.net/DATABASE"
 DEV_DB_URI_ADMIN="mongodb+srv://ADMIN:PASSWORD@prototype-db-hituj.mongodb.net/DATABASE"
@@ -21,27 +44,15 @@ DEV_DB_URI_ADMIN="mongodb+srv://ADMIN:PASSWORD@prototype-db-hituj.mongodb.net/DA
 PRO_DB_URI_USER="mongodb+srv://USER:PASSWORD@prototype-db-hituj.mongodb.net/DATABASE"
 PRO_DB_URI_ADMIN="mongodb+srv://ADMIN:PASSWORD@prototype-db-hituj.mongodb.net/DATABASE"
 
-## bcrypt hash salt rounds
-SALT_ROUNDS="10"
-
-## session cookie secret (used to sign the session ID cookie)
-SECRET_1="99F557F87E1CFD7CF5AB23F21E7D1"
-SECRET_2="5DC554673B99DD4B83BF8E766E722"
-SECRET_3="496A7AE21CDF3EC67217DAD89A1B1"
-
 ## reCaptcha keys:
+#   @see: https://www.google.com/recaptcha/admin#site/342399025?setup
 SITE_KEY=
 SECRET_KEY=
+# Keys for Invisible reCAPTCHA
+#   @see: https://developers.google.com/recaptcha/docs/invisible
+RECAP_INVIS_SITE_KEY=
+RECAP_INVIS_SECRET_KEY=
 
-## admin account
-#   root account within RPA that always exist
-ADMIN_USERNAME="admin"
-ADMIN_EMAIL="admin@email.com"
-ADMIN_PASSWORD="root"
-
-## Bot account
-#   bot account to verify emails
-BOT_USERNAME="rpa-bot"
-BOT_EMAIL="bot@email.com"
-BOT_EMAIL_AUTH="auth"
-BOT_PASSWORD="bot"
+## Email API key
+#   Note: this is only used once on start-up.
+SG_ADMIN_KEY=
